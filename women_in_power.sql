ef5b4accf712528a282a85ec31ddb8e6731257d3
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-11-09 21:32:36 -0600

    added files to db

--- women_in_power.sql
+++ women_in_power.sql
@@ -1,6 +1,6 @@
+drop table manager;
 create table manager (
 country varchar,
-country_name varchar,
 code varchar,
 new_code varchar primary key,
 latitude decimal,
@@ -19,4 +19,72 @@ indicator_name varchar,
 "2019" decimal,
 "2020" decimal
 );
-from manager 
+select * from manager;
+
+drop table labor;
+create table labor (
+country varchar,
+code varchar,
+new_code varchar primary key,
+latitude decimal,
+longitude decimal,
+indicator_name varchar,
+"2009" decimal,
+"2010" decimal,
+"2011" decimal,
+"2012" decimal,
+"2013" decimal,
+"2014" decimal,
+"2015" decimal,
+"2016" decimal,
+"2017" decimal,
+"2018" decimal,
+"2019" decimal,
+"2020" decimal
+);
+select * from labor;
+
+drop table ownership;
+create table ownership (
+country varchar,
+code varchar,
+new_code varchar primary key,
+latitude decimal,
+longitude decimal,
+indicator_name varchar,
+"2009" decimal,
+"2010" decimal,
+"2011" decimal,
+"2012" decimal,
+"2013" decimal,
+"2014" decimal,
+"2015" decimal,
+"2016" decimal,
+"2017" decimal,
+"2018" decimal,
+"2019" decimal,
+"2020" decimal
+);
+select * from ownership;
+
+drop table parliament;
+create table parliament (
+country varchar,
+code varchar,
+new_code varchar primary key,
+latitude decimal,
+longitude decimal,
+indicator_name varchar,
+"2009" decimal,
+"2010" decimal,
+"2011" decimal,
+"2012" decimal,
+"2013" decimal,
+"2014" decimal,
+"2015" decimal,
+"2016" decimal,
+"2017" decimal,
+"2018" decimal,
+"2019" decimal
+);
+select * from parliament;
