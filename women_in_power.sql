0f4c47f7600bd0909bd743f875718b07319d464d
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-11-09 20:41:38 -0600

    sql database

---
+++ women_in_power.sql
@@ -0,0 +1,22 @@
+create table manager (
+country varchar,
+country_name varchar,
+code varchar,
+new_code varchar primary key,
+latitude decimal,
+longitude decimal,
+indicator_name varchar,
+"2009" decimal,
+"2010" decimal,
+"2011" decimal,
+"2012" decimal,
+"2013" decimal,
+"2014" decimal,
+"2015" decimal,
+"2016" decimal,
+"2017" decimal,
+"2018" decimal,
+"2019" decimal,
+"2020" decimal
+);
+from manager 
